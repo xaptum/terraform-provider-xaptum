0a0ddfd38fbfd621afac77e35b50baef9ed28dd2
David R. Bild <david.bild@xaptum.com>
2019-08-27 15:06:19 -0500

    example: update rules

--- examples/firewall/main.tf
+++ examples/firewall/main.tf
@@ -12,6 +12,15 @@ resource "xaptum_enf_firewall_rule" "ex" {
   priority    = 1
   protocol    = "TCP"
   direction   = "EGRESS"
-  source_port = "22"
+  action      = "ACCEPT"
+}
+
+resource "xaptum_enf_firewall_rule" "ingress" {
+  network   = "2607:8f80:8080:8::/64"
+  ip_family = "IP6"
+
+  priority    = 1
+  protocol    = "TCP"
+  direction   = "INGRESS"
   action      = "ACCEPT"
 }
